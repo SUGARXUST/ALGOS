d57fa6a20980a28b833d62eeb9139825db826e4a
sugarlust <akashsourav780@gmail.com>
2022-06-22 02:40:00 +0530

    initial commit

--- ARRAYS/sliding window problem.cpp
+++ ARRAYS/sliding window problem.cpp
@@ -13,4 +13,39 @@ int fr[nmax];
 int slidingwindow(int a[], int n, int k)
 {
     int ans = 0, cnt = 0, right;
+    for (right = 1; right <= n; right++)
+    {
+        if (++fr[a[right]] == 1)
+            cnt++;
+        if (cnt > k)
+            break;
+    }
+    for (int left = 2; left <= n; left++)
+    {
+        if (--fr[a[left - 1]] == 0)
+            cnt--;
+        while (right < n)
+        {
+            if (++fr[a[right]] == 1)
+                cnt++;
+            if (cnt > k)
+                break;
+        }
+        if (cnt <= k)
+            return max(ans, n - left + 1);
+        if (--fr[a[left - 1]] == 0)
+            cnt--;
+        ans = max(ans, right - left + 1);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n, k, a[nmax];
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    cout << slidingwindow(a, n, k);
+    return 0;
 }
